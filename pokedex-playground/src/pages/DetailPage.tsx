8260716705b9d51facd26807f49564972b1694bc
dudeoeoeo <dudekekek@naver.com>
2022-01-21 18:03:22 +0900

    PokemonDetail

--- pokedex-playground/src/pages/DetailPage.tsx
+++ pokedex-playground/src/pages/DetailPage.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Tabs from '../components/Tabs';
 
 type Params = {
     id: string,
 }
 
+type Tab = 'about' | 'stats' | 'evolution';
+
 const DetailPage:React.FC = () => {
     const { id } = useParams<Params>();
+    const [selectedTab, setSelectedTab] = useState<Tab>('about');
+
+    const handleClick = (tab: Tab) => {
+        setSelectedTab(tab);
+    }
 
     return (
         <div>
-            DetailPage id: {id}
+            <Tabs tab={selectedTab} onClick={handleClick} color={{name: "red", url: ""}} />
         </div>
     );
 };
